b04e6ae7525b48497fccde821c7d019fffc80b7e
Leszek Lesner <leszek.lesner@web.de>
2018-12-11 18:21:21 +0100

    Initial commit

---
+++ usr/share/plasma/look-and-feel/org.kde.netrunner-black.desktop/contents/plasmoidsetupscripts/org.kde.plasma.kickerdash.js
@@ -0,0 +1,12 @@
+applet.currentConfigGroup = ["Shortcuts"]
+applet.writeConfig("global", "Alt+F1")
+
+applet.currentConfigGroup = ["General"]
+applet.writeConfig("customButtonImage", "file:///usr/share/icons/hicolor/scalable/apps/homerun.svg")
+applet.writeConfig("favoriteApps", ["org.kde.ksysguard.desktop", "systemsettings.desktop", "synaptic.desktop"])
+applet.writeConfig("limitDepth", true)
+applet.writeConfig("useCustomButtonImage", true)
+applet.writeConfig("alphaSort", true)
+applet.writeConfig("hiddenApplications", "org.kde.ksshaskpass.desktop,libreoffice-draw.desktop,libreoffice-base.desktop,libreoffice-math.desktop,lximage-qt-screenshot.desktop,audacious-qt.desktop,mpv.desktop,iceweasel.desktop")
+
+applet.reloadConfig();
