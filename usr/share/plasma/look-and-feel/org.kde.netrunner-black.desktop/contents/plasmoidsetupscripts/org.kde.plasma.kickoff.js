e2a34abb7c021148f779beba74c48a48488f5933
nulogicsystems <james@nulogicsystems.com>
2019-03-25 18:24:10 -0400

    Modifications
    Bring into line with needed Netrunner rolling changes

--- usr/share/plasma/look-and-feel/org.kde.netrunner-black.desktop/contents/plasmoidsetupscripts/org.kde.plasma.kickoff.js
+++ usr/share/plasma/look-and-feel/org.kde.netrunner-black.desktop/contents/plasmoidsetupscripts/org.kde.plasma.kickoff.js
@@ -3,8 +3,8 @@ applet.writeConfig("global", "Alt+F1")
 
 applet.currentConfigGroup = ["General"]
 applet.writeConfig("icon", "file:///usr/share/icons/hicolor/scalable/apps/homerun.svg")
-applet.writeConfig("favorites", ["org.kde.ksysguard.desktop", "systemsettings.desktop", "synaptic.desktop"])
+applet.writeConfig("favorites", ["org.kde.ksysguard.desktop", "systemsettings.desktop", "org.kde.discover", "octopi.desktop"])
 applet.writeConfig("alphaSort", true)
-applet.writeConfig("hiddenApplications", "org.kde.ksshaskpass.desktop,libreoffice-draw.desktop,libreoffice-base.desktop,libreoffice-math.desktop,lximage-qt-screenshot.desktop,audacious-qt.desktop,mpv.desktop,iceweasel.desktop")
+applet.writeConfig("hiddenApplications", "org.kde.ksshaskpass.desktop,libreoffice-draw.desktop,libreoffice-base.desktop,libreoffice-math.desktop,lximage-qt-screenshot.desktop,audacious-qt.desktop,mpv.desktop")
 
 applet.reloadConfig();
--- usr/share/plasma/look-and-feel/org.kde.netrunner-black.desktop/contents/plasmoidsetupscripts/org.kde.plasma.simplemenu.js
+++ usr/share/plasma/look-and-feel/org.kde.netrunner-black.desktop/contents/plasmoidsetupscripts/org.kde.plasma.simplemenu.js
@@ -3,9 +3,9 @@ applet.writeConfig("global", "Alt+F1")
 
 applet.currentConfigGroup = ["General"]
 applet.writeConfig("customButtonImage", "file:///usr/share/icons/hicolor/scalable/apps/homerun.svg")
-applet.writeConfig("favoriteApps", ["org.kde.ksysguard.desktop", "systemsettings.desktop", "synaptic.desktop"])
+applet.writeConfig("favoriteApps", ["org.kde.ksysguard.desktop", "systemsettings.desktop", "org.kde.discover", "octopi.desktop"])
 applet.writeConfig("useCustomButtonImage", true)
 applet.writeConfig("alphaSort", true)
-applet.writeConfig("hiddenApplications", "org.kde.ksshaskpass.desktop,libreoffice-draw.desktop,libreoffice-base.desktop,libreoffice-math.desktop,lximage-qt-screenshot.desktop,audacious-qt.desktop,mpv.desktop,iceweasel.desktop")
+applet.writeConfig("hiddenApplications", "org.kde.ksshaskpass.desktop,libreoffice-draw.desktop,libreoffice-base.desktop,libreoffice-math.desktop,lximage-qt-screenshot.desktop,audacious-qt.desktop,mpv.desktop")
 
 applet.reloadConfig();
